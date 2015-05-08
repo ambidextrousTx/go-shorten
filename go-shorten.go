f9e96f0394c03d4cea42862636b41fe6330b8c21
ambidextrousTx <ravisinha@my.unt.edu>
2015-05-07 23:33:29 -0500

    Clean up and add constants

--- go-shorten.go
+++ go-shorten.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"math"
 	"fmt"
-	"encoding/base64"
 )
 
-func test() {
-	fmt.Println("Called test()")
-
-}
-
 func main() {
-	// testing out base64 encoding of URLs
-	url := []byte("http://www.golang.com")
-	str := base64.StdEncoding.EncodeToString(url)
-	fmt.Println(str)
-	
-	fmt.Println(math.Cos(3.4))
-	test()
+	fmt.Println("Starting the URL shortening procedure")
+	const key uint16 = 125
+	const alphabetSize uint16 = 62
 
 }
