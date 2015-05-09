2b839511cedf8c560b6bf6ac02f7a461b7fc2b05
ambidextrousTx <ravisinha@my.unt.edu>
2015-05-09 07:52:22 -0500

    Add initial version of the convert function

--- go-shorten.go
+++ go-shorten.go
@@ -4,9 +4,22 @@ import (
 	"fmt"
 )
 
+/* Declate the return array in the signature itself.
+The function converts between bases and reverses the
+result before returning it */
+func convert(key, alphabet uint16) (digits []uint16){
+    for num := key; num > 0; num = num / key {
+        remainder := num % key
+        digits = append(digits, remainder)
+    }
+    // TODO: Reverse digits
+    // TODO: Fix bugs
+    return
+}
+
 func main() {
 	fmt.Println("Starting the URL shortening procedure")
 	const key uint16 = 125
 	const alphabetSize uint16 = 62
-
+	fmt.Println("Converted 125_10 to X_62,", convert(key, alphabetSize))
 }
