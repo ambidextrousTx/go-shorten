1af42dd4e0968133f3254d1b952b5b214072266d
ambidextrousTx <ravisinha@my.unt.edu>
2015-05-19 23:43:22 -0500

    Add stub code for the mapping between digits and the alphabet

--- go-shorten.go
+++ go-shorten.go
@@ -1,16 +1,19 @@
+/* Implement a URL shortening algorithm in GoLang based on a tutorial
+on Stack Overflow */
+
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 /* Reverse an array of digits. Implemented natively for now, although
 there might be an in-built library function that does this already */
 func reverse(digits []uint16) (reversed []uint16) {
     for i := len(digits) - 1; i >= 0; i-- {
-		reversed = append(reversed, digits[i])
-	}
-	return
+        reversed = append(reversed, digits[i])
+    }
+    return
 }
 
 /* Declare the return array in the signature itself. The function
@@ -23,9 +26,22 @@ func convert(key, alphabetSize uint16) (digits []uint16){
     return reverse(digits)
 }
 
+/* Map the indices obtained from the convert and reverse functions
+above into our alphabet. The alphabet is a-zA-Z0-9 */
+func mapToAlphabet(digits []uint16, alphabetMap map[uint16]string) string {
+    var shortUrl string
+    for _, digit := range digits {
+        shortUrl += alphabetMap[digit]
+        fmt.Println(digit)
+    }
+    return shortUrl
+}
+
 func main() {
-	fmt.Println("Starting the URL shortening procedure")
-	const key uint16 = 125
-	const alphabetSize uint16 = 62
-	fmt.Println("Converted 125_10 to X_62,", convert(key, alphabetSize))
+    fmt.Println("Starting the URL shortening procedure")
+    const key uint16 = 125
+    const alphabetSize uint16 = 62
+    var alphabetMap map[uint16]string= make(map[uint16]string, alphabetSize)
+    fmt.Println("Converted 125_10 to X_62,", convert(key, alphabetSize))
+    fmt.Println(mapToAlphabet(convert(key, alphabetSize), alphabetMap))
 }
